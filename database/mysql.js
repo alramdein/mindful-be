7ae311431312ec846c80b312a885dcae4c8fc5de
alramdein <ramdanialif26@gmail.com>
2021-11-16 20:47:20 +0700

    refactor: handleDisconnect db 2

--- database/mysql.js
+++ database/mysql.js
@@ -1,7 +1,9 @@
 const mysql = require("mysql");
 
+let connection;
+
 const handleDisconnect = () => {
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
