5e97c1edc25161bf5c6f96030269318a61bb316f
alramdein <ramdanialif26@gmail.com>
2021-11-16 20:45:42 +0700

    refactor: handleDisconnect db

--- database/mysql.js
+++ database/mysql.js
@@ -1,30 +1,30 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
+const handleDisconnect = () => {
+  const connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  });
 
-// const handleDisconnect = () => {
-//   connection.connect((err) => {
-//     if (err) {
-//       console.log("error when connecting to db:", err);
-//       setTimeout(handleDisconnect, 2000);
-//     }
-//   });
+  // connection.connect((err) => {
+  //   if (err) {
+  //     console.log("error when connecting to db:", err);
+  //     setTimeout(handleDisconnect, 2000);
+  //   }
+  // });
 
-//   connection.on("error", (err) => {
-//     console.log("db error", err);
-//     if (err.code === "PROTOCOL_CONNECTION_LOST") {
-//       handleDisconnect();
-//     } else {
-//       throw err;
-//     }
-//   });
-// };
+  connection.on("error", (err) => {
+    console.log("db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+};
 
-// handleDisconnect();
+handleDisconnect();
 
 module.exports = connection;
